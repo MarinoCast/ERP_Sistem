93d51b916b39225a68bb93a752524c59e8ccabde
Marino D' Jesus <39675612+MarinoCast@users.noreply.github.com>
2022-08-21 21:36:19 -0400

    Add project files.

---
+++ ERP_System_Api/Program.cs
@@ -0,0 +1,36 @@
+using ERP_System_Api;
+using Microsoft.Extensions.DependencyInjection;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+//Configuration Instance
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var swaggerOptions = new SwaggerOptions();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.MapControllers();
+
+app.Run();
---
+++ ERP_System_Api/Services/ICrudServices.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ERP_System_Api
+{
+    public interface ICrudServices<T> where T : class
+    {
+        Task<T> Create(T t);
+
+        Task<List<T>> Get();
+
+        Task<T> GetById(long id);
+
+        Task<T> Update(T t);
+
+        Task<bool> Delete(long id);
+    }
+}
